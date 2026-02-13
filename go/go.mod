a5e4c55a2f18bc21f2b076a828a455ca343e9a81
Ingy döt Net <ingy@ingy.net>
2026-02-13 12:59:23 -0800

    Add go sources for 'go install' installs

---
+++ go/go.mod
@@ -0,0 +1,13 @@
+module github.com/gloathub/go
+
+go 1.24
+
+require github.com/gloathub/glojure v0.6.5-rc4
+
+require (
+	bitbucket.org/pcastools/hash v1.0.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	go4.org/intern v0.0.0-20220617035311-6925f38cc365 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20230525183740-e7c30c78aeb2 // indirect
+)
---
+++ go/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"github.com/gloathub/glojure/pkg/glj"
+	"github.com/gloathub/glojure/pkg/lang"
+	_ "github.com/gloathub/go/pkg/gist/core"
+	_ "github.com/gloathub/go/pkg/yamlscript/common"
+	_ "github.com/gloathub/go/pkg/yamlscript/util"
+	_ "github.com/gloathub/go/pkg/ys/fs"
+	_ "github.com/gloathub/go/pkg/ys/http"
+	_ "github.com/gloathub/go/pkg/ys/ipc"
+	_ "github.com/gloathub/go/pkg/ys/json"
+	_ "github.com/gloathub/go/pkg/ys/std"
+	_ "github.com/gloathub/go/pkg/ys/dwim"
+	_ "github.com/gloathub/go/pkg/ys/v0"
+)
+
+func main() {
+	require := glj.Var("clojure.core", "require")
+	require.Invoke(lang.NewSymbol("yamlscript.common"))
+	require.Invoke(lang.NewSymbol("yamlscript.util"))
+	require.Invoke(lang.NewSymbol("ys.fs"))
+	require.Invoke(lang.NewSymbol("ys.http"))
+	require.Invoke(lang.NewSymbol("ys.ipc"))
+	require.Invoke(lang.NewSymbol("ys.json"))
+	require.Invoke(lang.NewSymbol("ys.std"))
+	require.Invoke(lang.NewSymbol("ys.dwim"))
+	require.Invoke(lang.NewSymbol("ys.v0"))
+	require.Invoke(lang.NewSymbol("gist.core"))
+
+	// Set up dynamic variables
+	alterVarRoot := glj.Var("clojure.core", "alter-var-root")
+	constantly := glj.Var("clojure.core", "constantly")
+
+	// ENV: map of all environment variables
+	environ := os.Environ()
+	envPairs := make([]any, 0, len(environ)*2)
+	for _, e := range environ {
+		if idx := strings.IndexByte(e, '='); idx >= 0 {
+			envPairs = append(envPairs, e[:idx], e[idx+1:])
+		}
+	}
+	envVar := glj.Var("gist.core", "ENV")
+	alterVarRoot.Invoke(envVar, constantly.Invoke(lang.NewMap(envPairs...)))
+
+	// NS: the user's namespace object
+	nsVar := glj.Var("gist.core", "NS")
+	nsObj := lang.FindOrCreateNamespace(lang.NewSymbol("gist.core"))
+	alterVarRoot.Invoke(nsVar, constantly.Invoke(nsObj))
+
+	// Set *ns* to the user's namespace using thread bindings
+	nsStarVar := glj.Var("clojure.core", "*ns*")
+	pushBindings := glj.Var("clojure.core", "push-thread-bindings")
+	bindings := lang.NewMap(nsStarVar, nsObj)
+	pushBindings.Invoke(bindings)
+
+	// CWD: current working directory
+	cwd, _ := os.Getwd()
+	cwdVar := glj.Var("gist.core", "CWD")
+	alterVarRoot.Invoke(cwdVar, constantly.Invoke(cwd))
+
+	// RUN: runtime metadata map (includes args and pid)
+	args := os.Args[1:]
+	anyArgs := make([]any, len(args))
+	for i, arg := range args {
+		anyArgs[i] = arg
+	}
+	argsVec := lang.NewVector(anyArgs...)
+	runMap := lang.NewMap(
+		lang.NewKeyword("args"), argsVec,
+		lang.NewKeyword("pid"), int64(os.Getpid()),
+	)
+	runVar := glj.Var("gist.core", "RUN")
+	alterVarRoot.Invoke(runVar, constantly.Invoke(runMap))
+
+	// ARGV and ARGS are set in -main function itself
+	// Call -main with args
+	myMain := glj.Var("gist.core", "-main")
+	myMain.Invoke(anyArgs...)
+}
